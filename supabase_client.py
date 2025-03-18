6f52770252f0c962dd6c87746f3fc3c27dce5188
Juan Carlos Montenegro Gonzales <juan.montenegro@jnj.gob.pe>
2025-03-18 17:01:58 -0500

    Primer commit: Inicialización del proyecto

---
+++ supabase_client.py
@@ -0,0 +1,6 @@
+from supabase import create_client
+
+SUPABASE_URL = "https://tusupabase.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqcGNmdXl6aHJucWNqeG55eGNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwNzUzODQsImV4cCI6MjA1NDY1MTM4NH0.vASDpURRLt5wf9ngGkCNkiT0c-XbillDLTW9KFqk9Tg"
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
